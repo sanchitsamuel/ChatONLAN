53e75acb84ea3fac1d2bc42a3480a264cfe90157
Sanchit Samuel <sanchit.samuel@live.com>
2016-06-14 23:57:15 +0530

    Added sockets for communication (basic)

--- src_mainwindow.py
+++ src_mainwindow.py
@@ -17,6 +17,7 @@ class ChatONLAN(QMainWindow, Ui_MainWindow):
     FAV = QTreeWidgetItem()
     settings = QSettings('chatonlan', 'config')
     open_chat_list = {}
+    open_socket = {}
 
     def __init__(self):
         super(ChatONLAN, self).__init__()
@@ -80,80 +81,125 @@ class ChatONLAN(QMainWindow, Ui_MainWindow):
             self.open_chat_list.pop(text)
 
     def tab_changed(self, index):
-        send = self.tabWidget.widget(index).findChildren(QPushButton, "send")
-        send[0].clicked.connect(self.send_button_pressed)
+        if index != 0:
+            send = self.tabWidget.widget(index).findChildren(QPushButton, "send")
+            send[0].clicked.connect(self.send_button_pressed)
+            send_default = self.tabWidget.widget(index).findChildren(QCheckBox, "send_default")
+            send_default[0].stateChanged.connect(self.checkbox_state_changed)
 
     def tree_double_clicked(self, item, column):
         text = item.text(0)
         if text != 'Online':
             if text != 'Favorites':
-                if text in self.open_chat_list:
-                    index = self.open_chat_list[text]
-                    self.tabWidget.setCurrentIndex(index)
-                    self.tab_changed(index)
-                else:
-                    chat_box = QTextEdit()
-                    chat_box.setObjectName('chat_box')
-                    chat_box.setReadOnly(True)
-                    chat_msg = QLineEdit()
-                    chat_msg.setObjectName('chat_msg')
-                    msg_send = QPushButton('Send')
-                    msg_send.setObjectName('send')
+                self.create_tab(text, True)
 
-                    chat_widget = QWidget()
+    def create_tab(self, name, switch=False, get_tab_number=False):
+        if name in self.open_chat_list:
+            index = self.open_chat_list[text]
+            if switch:
+                self.tabWidget.setCurrentIndex(index)
+                self.tab_changed(index)
+            if get_tab_number:
+                return index
+        else:
+            chat_box = QTextEdit()
+            chat_box.setObjectName('chat_box')
+            chat_box.setReadOnly(True)
+            chat_msg = QLineEdit()
+            chat_msg.setObjectName('chat_msg')
+            msg_send = QPushButton('Send')
+            msg_send.setObjectName('send')
+            send_default = QCheckBox('Enter to send message')
+            send_default.setObjectName('send_default')
 
-                    hBox_layout = QHBoxLayout()
-                    chat_tab_layout = QVBoxLayout()
-                    hBox_layout.addWidget(chat_msg)
-                    hBox_layout.addWidget(msg_send)
-                    chat_tab_layout.addWidget(chat_box)
-                    chat_tab_layout.addLayout(hBox_layout)
+            chat_widget = QWidget()
 
-                    chat_widget.setLayout(chat_tab_layout)
-                    chat_widget.setTabOrder(chat_box, chat_msg)
-                    chat_widget.setTabOrder(chat_msg, msg_send)
+            hBox_layout = QHBoxLayout()
+            chat_tab_layout = QVBoxLayout()
+            hBox_layout.addWidget(chat_msg)
+            hBox_layout.addWidget(msg_send)
+            chat_tab_layout.addWidget(chat_box)
+            chat_tab_layout.addLayout(hBox_layout)
+            chat_tab_layout.addWidget(send_default)
 
-                    index = self.tabWidget.addTab(chat_widget, text)
-                    self.tabWidget.setCurrentIndex(index)
-                    text = self.tabWidget.tabText(index)
-                    self.open_chat_list[text] = index
-                    self.tab_changed(index)
+            chat_widget.setLayout(chat_tab_layout)
+            chat_widget.setTabOrder(chat_box, chat_msg)
+            chat_widget.setTabOrder(chat_msg, msg_send)
+
+            index = self.tabWidget.addTab(chat_widget, name)
+            if switch:
+                self.tabWidget.setCurrentIndex(index)
+                self.open_chat_list[name] = index
+                self.tab_changed(index)
+                # self.create_socket(name)
+            if get_tab_number:
+                return index
+
+            # self.create_socket(name)
+
+    def create_socket(self, name):
+        s_msg = socket(AF_INET, SOCK_STREAM)
+        s_msg.connect((self.MEMBERS[name], 9000))
+        self.open_socket[name] = s_msg
 
     def get_name_from_address(self, address):
         return list(self.MEMBERS.keys())[list(self.MEMBERS.values()).index(address)]
 
+    def start_receive(self):
+        recv = threading.Thread(target=self.receive_message)
+        recv.setDaemon(True)
+        recv.start()
+
     def receive_message(self):
         r_msg = socket(AF_INET, SOCK_STREAM)
         r_msg.bind(('', 9000))
-        r_msg.listen(1)
-        connection, address = r_msg.accept()
+        r_msg.listen(20)
         while self.running:
+            connection, address = r_msg.accept()
             if address:
-                data, address = connection.recvfrom(4096)
-                self.display_message(data, address)
+                name = self.get_name_from_address(address)
+                if name:
+                    self.open_socket[name] = connection
+                    data, address = connection.recvfrom(4096)
+                    self.display_message(data, address)
 
     def display_message(self, data, address):
+        # maybe not be used later
         name = self.get_name_from_address(address)
         if name in self.open_chat_list:
-            tab_number = self.open_chat_list[name]
+            index = self.create_tab(name)
+            chat_box = self.tabWidget.widget(index).findChildren(QTextEdit, "chat_box")
+            chat_box[0].append(data)
         else:
             pass
 
+    def checkbox_state_changed(self, state):
+        send = self.tabWidget.widget(self.tabWidget.currentIndex()).findChildren(QPushButton, "send")
+        if state == 1:
+            send[0].setDefault(True)
+        else:
+            send[0].setDefault(False)
+
     def send_button_pressed(self):
+        # get tab label and from that the IP and send it to the function
+        address = self.tabWidget.tabText(self.tabWidget.currentIndex())
         find_widget = self.tabWidget.widget(self.tabWidget.currentIndex()).findChildren(QLineEdit, "chat_msg")
         chat_msg = find_widget[0]
         if chat_msg.isModified():
-            self.send_message(chat_msg.text())
+            self.send_message(chat_msg.text(), address)
             chat_msg.clear()
         else:
             self.statusbar.showMessage('Type a message to send.')
 
-    def send_message(self, msg, to=None):
+    def send_message(self, msg, to):
+        # sock = self.open_socket[to]
         username = self.settings.value('username', type=str)
+        to = to.replace('&', '')
         find_widget = self.tabWidget.widget(self.tabWidget.currentIndex()).findChildren(QTextEdit, "chat_box")
         chat_box = find_widget[0]
-        to_display = '<b>' + username + '</b>: ' + msg
+        to_display = '<font color="blue"><b>' + username + '</b>: ' + msg + '</font>'
         chat_box.append(to_display)
+        # sock.send(bytes(msg))
 
     def broadcast(self):
         username = self.settings.value('username', type=str)
@@ -254,6 +300,7 @@ if __name__ == '__main__':
     else:
         w.start_beacon()
         w.start_member_lookup()
+        w.start_receive()
 
         w.show()
         sys.exit(app.exec_())
